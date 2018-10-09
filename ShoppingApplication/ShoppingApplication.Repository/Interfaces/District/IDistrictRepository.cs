a921ce29ea693b5e10a3b0108947d99c9af2447a
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-10-09 18:10:50 +0300

    Added interfaces for repository.

---
+++ ShoppingApplication/ShoppingApplication.Repository/Interfaces/District/IDistrictRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingApplication.Repository.Interfaces.District
+{
+    public interface IDistrictRepository: IRepositoryBase<Data.Models.District>
+    {
+    }
+}
---
+++ ShoppingApplication/ShoppingApplication.Repository/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShoppingApplication.Repository.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        #region Get
+        IQueryable<T> GetAll();
+        T GetItem(int id);
+        #endregion
+
+        #region Find
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        T Find(Expression<Func<T, bool>> match);
+        #endregion
+
+        #region Delete
+        void Delete(T entity);
+        void Delete(int entityId);
+        #endregion
+
+        #region Add
+        void Create(T entity);
+        #endregion
+
+        #region Save
+        void SaveChanges();
+        #endregion
+
+    }
+}
